63721c73854791b3511450415db8c8c05eae447b
sai teja <76621108+sai-teja-git@users.noreply.github.com>
2024-03-21 14:42:59 +0000

    feat:Added guage chart

--- src/pages/user-pages/Dashboard.tsx
+++ src/pages/user-pages/Dashboard.tsx
@@ -11,16 +11,18 @@ export default function Dashboard() {
 
     const [load_overall_data, setOverallDataLoader] = useState(true);
     const [overall_card_details, setOverallCardDetails] = useState<Record<string, any>>({});
-    const [chart_options, setChartOptions] = useState({})
+    const [month_spend_chart, setChartOptions] = useState({})
+    const [overall_gauge, setOverallGaugeChart] = useState({})
 
     useEffect(() => {
         setTimeout(() => {
             setOverallDataLoader(false)
         }, 1000)
-        setChart()
+        setLineChart();
+        setGaugeChart()
     }, [])
 
-    function setChart() {
+    function setLineChart() {
         let spends_till_now = 0;
         let categ_arr: any[] = [];
         let spends_arr: any[] = [];
@@ -87,124 +89,122 @@ export default function Dashboard() {
         setChartOptions(options)
     }
 
-    function overallDataCard() {
-        if (load_overall_data) {
-            return <>
-                <div className="overall-container placeholder-glow">
-                    {
-                        Array(5).fill(null).map((e, i) => (
-                            <div className="overall-block" key={i} >
-                                <div className="block-left">
-                                    <div className="icon">
-                                        <span className="placeholder col-2"></span>
-                                    </div>
-                                </div>
-                                <div className="block-right">
-                                    <div className="amount">
-                                        <span className="placeholder col-6"></span>
-                                    </div>
-                                    <div className="name">
-                                        <span className="placeholder col-7"></span>
-                                    </div>
-                                    <div className="remarks">
-                                        <span className="placeholder col-6"></span>
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-            </>
-        } else {
-            return <>
-                <div className="overall-container">
-                    <div className="overall-block spend">
-                        <div className="block-left">
-                            <div className="icon">
-                                <i className="fa-solid fa-money-bill-trend-up"></i>
-                            </div>
-                        </div>
-                        <div className="block-right">
-                            <div className="amount">
-                                <Currency value={overall_card_details["total_spends"] ? overall_card_details["total_spends"] : 0} />
-                            </div>
-                            <div className="name">
-                                Spends
-                            </div>
-                            <div className="remarks"></div>
-                        </div>
-                    </div>
-                    <div className="overall-block estimation">
-                        <div className="block-left">
-                            <div className="icon">
-                                <i className="fa-solid fa-file-invoice"></i>
-                            </div>
-                        </div>
-                        <div className="block-right">
-                            <div className="amount">
-                                <Currency value={overall_card_details["total_estimations"] ? overall_card_details["total_estimations"] : 0} />
-                            </div>
-                            <div className="name">
-                                Estimation
-                            </div>
-                            <div className="remarks"></div>
-                        </div>
-                    </div>
-                    <div className="overall-block income">
-                        <div className="block-left">
-                            <div className="icon">
-                                <i className="fa-solid fa-hand-holding-dollar"></i>
-                            </div>
-                        </div>
-                        <div className="block-right">
-                            <div className="amount">
-                                <Currency value={overall_card_details["total_income"] ? overall_card_details["total_income"] : 0} />
-                            </div>
-                            <div className="name">
-                                Income
-                            </div>
-                            <div className="remarks"></div>
-                        </div>
-                    </div>
-                    <div className="overall-block max">
-                        <div className="block-left">
-                            <div className="icon">
-                                <i className="fa-solid fa-arrow-trend-up"></i>
-                            </div>
-                        </div>
-                        <div className="block-right">
-                            <div className="amount">
-                                <Currency value={overall_card_details["max_spend"] ? overall_card_details["max_spend"] : 0} />
-                            </div>
-                            <div className="name">
-                                Month Max Spends
-                            </div>
-                            <div className="remarks">
-                                {/* On {{ user_categories_object[overall_details['max_spend_on_category']].name }} */}
-                                On Home
-                            </div>
-                        </div>
-                    </div>
-                    <div className="overall-block year-total">
-                        <div className="block-left">
-                            <div className="icon">
-                                <i className="fa-solid fa-chart-bar"></i>
-                            </div>
-                        </div>
-                        <div className="block-right">
-                            <div className="amount">
-                                <Currency value={overall_card_details["spend_avg"] ? overall_card_details["spend_avg"] : 0} />
-                            </div>
-                            <div className="name">
-                                Month Average Spends
-                            </div>
-                            <div className="remarks">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </>
+    function setGaugeChart() {
+        const colors = ["#feb56a", "#fa4b42", "#00e272"]
+        const options = {
+
+            chart: {
+                type: 'solidgauge',
+                height: '110%',
+                // events: {
+                //     render: renderIcons
+                // }
+            },
+
+            title: {
+                text: 'Multiple KPI gauge',
+                style: {
+                    fontSize: '24px'
+                }
+            },
+
+            tooltip: {
+                borderWidth: 0,
+                backgroundColor: 'none',
+                shadow: false,
+                style: {
+                    fontSize: '16px'
+                },
+                valueSuffix: '%',
+                pointFormat: '{series.name}<br>' +
+                    '<span style="font-size: 2em; color: {point.color}; ' +
+                    'font-weight: bold">{point.y}</span>',
+                // positioner: function (labelWidth) {
+                //     return {
+                //         x: (this.chart.chartWidth - labelWidth) / 2,
+                //         y: (this.chart.plotHeight / 2) + 15
+                //     };
+                // }
+            },
+
+            pane: {
+                startAngle: 0,
+                endAngle: 360,
+                background: [{ // Track for Conversion
+                    outerRadius: '112%',
+                    innerRadius: '88%',
+                    backgroundColor: colors[0] + "4d",
+                    borderWidth: 0
+                }, { // Track for Engagement
+                    outerRadius: '87%',
+                    innerRadius: '63%',
+                    backgroundColor: colors[1] + "4d",
+                    borderWidth: 0
+                }, { // Track for Feedback
+                    outerRadius: '62%',
+                    innerRadius: '38%',
+                    backgroundColor: colors[2] + "4d",
+                    borderWidth: 0
+                }]
+            },
+
+            yAxis: {
+                min: 0,
+                max: 100,
+                lineWidth: 0,
+                tickPositions: []
+            },
+
+            plotOptions: {
+                solidgauge: {
+                    dataLabels: {
+                        enabled: false
+                    },
+                    linecap: 'round',
+                    stickyTracking: false,
+                    rounded: true
+                }
+            },
+
+            series: [{
+                name: 'Conversion',
+                data: [{
+                    color: colors[0],
+                    radius: '112%',
+                    innerRadius: '88%',
+                    y: 80
+                }],
+                custom: {
+                    icon: 'filter',
+                    iconColor: '#303030'
+                }
+            }, {
+                name: 'Engagement',
+                data: [{
+                    color: colors[1],
+                    radius: '87%',
+                    innerRadius: '63%',
+                    y: 65
+                }],
+                custom: {
+                    icon: 'comments-o',
+                    iconColor: '#ffffff'
+                }
+            }, {
+                name: 'Feedback',
+                data: [{
+                    color: colors[2],
+                    radius: '62%',
+                    innerRadius: '38%',
+                    y: 50
+                }],
+                custom: {
+                    icon: 'commenting-o',
+                    iconColor: '#303030'
+                }
+            }]
         }
+        setOverallGaugeChart(options)
     }
 
     function overallDataNewCard() {
@@ -287,9 +287,9 @@ export default function Dashboard() {
                 <div className="row m-0 mt-3">
                     <div className="col-12 p-0">
                         <div className="card">
-                            <div className="card-body" style={{}}>
+                            <div className="card-body">
                                 <div>
-                                    <HighchartsReact containerProps={{ className: "day-wise-spends" }} highcharts={Highcharts} options={chart_options} />
+                                    <HighchartsReact containerProps={{ className: "day-wise-spends" }} highcharts={Highcharts} options={month_spend_chart} />
                                 </div>
                             </div>
                         </div>
@@ -359,7 +359,11 @@ export default function Dashboard() {
                             </div>
                             <div className="pie-graph col-xl-12 col-lg-12 col-md-6 col-sm-12 pe-0 ps-xl-0 ps-lg-0 ps-md-2 ps-0 mt-xl-3 mt-lg-3 mt-md-0 mt-3">
                                 <div className="card">
-                                    <div className="card-body"></div>
+                                    <div className="card-body">
+                                        <div>
+                                            {/* <HighchartsReact containerProps={{ className: "day-wise-spends" }} highcharts={Highcharts} options={overall_gauge} /> */}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
